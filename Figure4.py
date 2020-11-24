6881e37ce78da7f35c717832168930f1887a18db
Saur0pony <73480879+Saur0pony@users.noreply.github.com>
2020-11-24 15:46:35 +0100

    Refonte du fichier de figure 4
    Amélioration avec ajout de légendes et de commentaires visant à mieux comprendre.
    Superposition des courbes pour Q =1 et Q = 0 
    Reste encore en bordel, mais avancée actuelle mise à jour.
    On remarque bien la différence sur la ttf entre Q = 1 (pics plus important et plus fin) 
    Encore des soucis liés à cette ttf (malgré une augmentation du tau, les pics restent serrés avec peu de points (donc succession de segments)

--- Figure4.py
+++ Figure4.py
@@ -16,19 +16,23 @@ k_s0 = 1
 k_s1 = 0.01
 t = 4500
 eta = 2.0
-Q = 0
 
-D = 75
+D = 90
 
 beta = []
 for i in range(nCell):
     beta.append(random.gauss(1,0.05))
 tau = 0.04 ##a voir
 
+'''
+Réalisation de la simulation pour Q = 1, les cellules sont en synchro parfaite.
+'''
+Q = 1
+
 bar = Bar("Progress", max=t-1)
 ##initialisation des matrices
 a = np.zeros((nCell, t))
-b = np.zeros((nCell, t))
+bQ1 = np.zeros((nCell, t))
 c = np.zeros((nCell, t))
 
 A = np.zeros((nCell, t))
@@ -38,9 +42,8 @@ C = np.zeros((nCell, t))
 S = np.zeros((nCell, t))
 Se = np.zeros((nCell, t))
 
-
 a[:,0] = 0
-b[:,0] = 50
+bQ1[:,0] = 50
 c[:,0] = 0
 
 A[:,0] = 0
@@ -50,37 +53,6 @@ C[:,0] = 0
 S[:,0] = 0
 Se[:,0] = 0
 
-"""
-a[:,0] = random.randint(1,100)
-b[:,0] = random.randint(1,100)
-c[:,0] = random.randint(1,100)
-A[:,0] = random.randint(1,100)
-B[:,0] = random.randint(1,100)
-C[:,0] = random.randint(1,100)
-S[:,0] = random.randint(1,100)
-Se[:,0] = 0
-"""
-"""
-print(a[:,0])
-print(b[:,0])
-print(c[:,0])
-print(A[:,0])
-print(B[:,0])
-print(C[:,0])
-"""
-
-"""
-a[:,0] = 0
-b[:,0] = 0
-c[:,0] = 0
-A[:,0] = 0
-B[:,0] = 0
-C[:,0] = 0
-S[:,0] = 0
-Se[:,0] = 0
-"""
-
-frequence_list = []
 
 time = np.arange(0,t)
 
@@ -92,17 +64,78 @@ for j in range(0, t-1):
         Se[i, j] = Q * np.mean(S[:, j])
 
         abis = a[i, j] + tau / 2 * (-a[i, j] + (alpha / (1 + C[i, j] ** n)))
-        bbis = b[i, j] + tau / 2 * (-b[i, j] + (alpha / (1 + A[i, j] ** n)))
+        bbis = bQ1[i, j] + tau / 2 * (-bQ1[i, j] + (alpha / (1 + A[i, j] ** n)))
         cbis = c[i, j] + tau / 2 * (-c[i, j] + (alpha / (1 + B[i, j] ** n)) + (kappa * S[i, j] / 1 + S[i, j]))
 
         Abis = A[i, j] + tau / 2 * (betabis * (a[i, j] - A[i, j]))
-        Bbis = B[i, j] + tau / 2 * (betabis * (b[i, j] - B[i, j]))
+        Bbis = B[i, j] + tau / 2 * (betabis * (bQ1[i, j] - B[i, j]))
         Cbis = C[i, j] + tau / 2 * (betabis * (c[i, j] - C[i, j]))
 
         Sbis = S[i, j] + tau / 2 * ((-k_s0 * S[i, j]) + (k_s1 * A[i, j]) - (eta * (S[i, j] - Se[i, j])))
 
         a[i, j + 1] = a[i, j] + tau * (-abis + (alpha / (1 + Cbis ** n)))
-        b[i, j + 1] = b[i, j] + tau * (-bbis + (alpha / (1 + Abis ** n)))
+        bQ1[i, j + 1] = bQ1[i, j] + tau * (-bbis + (alpha / (1 + Abis ** n)))
+        c[i, j + 1] = c[i, j] + tau * (-cbis + (alpha / (1 + Bbis ** n)) + (kappa * Sbis / 1 + Sbis))
+
+        A[i, j + 1] = A[i, j] + tau * (betabis * (abis - Abis))
+        B[i, j + 1] = B[i, j] + tau * (betabis * (bbis - Bbis))
+        C[i, j + 1] = C[i, j] + tau * (betabis * (cbis - Cbis))
+
+        S[i, j + 1] = S[i, j] + tau * ((-k_s0 * Sbis) + (k_s1 * Abis) - (eta * (Sbis - Se[i, j])))
+
+    bar.next()
+
+'''
+Réalisation de la simulation pour Q = 0, les cellules sont en synchro parfaite.
+'''
+Q = 0
+
+bar = Bar("Progress", max=t-1)
+##initialisation des matrices
+a = np.zeros((nCell, t))
+bQ2 = np.zeros((nCell, t))
+c = np.zeros((nCell, t))
+
+A = np.zeros((nCell, t))
+B = np.zeros((nCell, t))
+C = np.zeros((nCell, t))
+
+S = np.zeros((nCell, t))
+Se = np.zeros((nCell, t))
+
+a[:,0] = 0
+bQ2[:,0] = 50
+c[:,0] = 0
+
+A[:,0] = 0
+B[:,0] = 0
+C[:,0] = 0
+
+S[:,0] = 0
+Se[:,0] = 0
+
+
+time = np.arange(0,t)
+
+for j in range(0, t-1):
+    for i in range(0, nCell):
+        xi = random.gauss(0, D)
+        betabis = beta[i] + (xi*tau)
+
+        Se[i, j] = Q * np.mean(S[:, j])
+
+        abis = a[i, j] + tau / 2 * (-a[i, j] + (alpha / (1 + C[i, j] ** n)))
+        bbis = bQ2[i, j] + tau / 2 * (-bQ2[i, j] + (alpha / (1 + A[i, j] ** n)))
+        cbis = c[i, j] + tau / 2 * (-c[i, j] + (alpha / (1 + B[i, j] ** n)) + (kappa * S[i, j] / 1 + S[i, j]))
+
+        Abis = A[i, j] + tau / 2 * (betabis * (a[i, j] - A[i, j]))
+        Bbis = B[i, j] + tau / 2 * (betabis * (bQ2[i, j] - B[i, j]))
+        Cbis = C[i, j] + tau / 2 * (betabis * (c[i, j] - C[i, j]))
+
+        Sbis = S[i, j] + tau / 2 * ((-k_s0 * S[i, j]) + (k_s1 * A[i, j]) - (eta * (S[i, j] - Se[i, j])))
+
+        a[i, j + 1] = a[i, j] + tau * (-abis + (alpha / (1 + Cbis ** n)))
+        bQ2[i, j + 1] = bQ2[i, j] + tau * (-bbis + (alpha / (1 + Abis ** n)))
         c[i, j + 1] = c[i, j] + tau * (-cbis + (alpha / (1 + Bbis ** n)) + (kappa * Sbis / 1 + Sbis))
 
         A[i, j + 1] = A[i, j] + tau * (betabis * (abis - Abis))
@@ -114,47 +147,45 @@ for j in range(0, t-1):
     bar.next()
 
 
-btot = []
+btotQ1 = []
 for i in range(0, t):
     binter = 0
     for j in range(nCell):
-        binter += b[j,i]
-    btot.append(binter/nCell)
+        binter += bQ1[j,i]
+    btotQ1.append(binter/nCell)
 
-theMaxOfbtot = 0
-for num in btot:
-    if num > theMaxOfbtot:
-        theMaxOfbtot = num
-#print(btot)
-btotnorm = (btot - (theMaxOfbtot/2))/(theMaxOfbtot/2)
-b1norm = (b[1, :] - (max(b[1, :])/2))/(max(b[1, :])/2)
-#print(btot)
-#print(len(btot))
+btotQ2 = []
+for i in range(0, t):
+    binter = 0
+    for j in range(nCell):
+        binter += bQ2[j,i]
+    btotQ2.append(binter/nCell)
+
+#Centralisation de btot et b1
+btotQ1norm = (btotQ1 - (max(btotQ1)/2))/(max(btotQ1)/2)
+b1Q1norm = (bQ1[1, :] - (max(bQ1[1, :])/2))/(max(bQ1[1, :])/2)
+
+btotQ2norm = (btotQ2 - (max(btotQ2)/2))/(max(btotQ2)/2)
+b1Q2norm = (bQ2[1, :] - (max(bQ2[1, :])/2))/(max(bQ2[1, :])/2)
 
 
-#calcul du décallage de fréquence
-PSDlist = []
-PSDmax = 0
-#PSDfreq = []
-for i in range(0, t-1):
-    PSD = 0
-    for j in range(0, t-i):
-            PSD += btotnorm[j] * btotnorm[j+i]
-    PSDlist.append(PSD)
-    if PSD > PSDmax:
-        PSDmax = PSD
+#Autocorrelation des valeurs : calcul la somme des multiplications des valeurs aux temps avec un décallage (b(t) * b(t+i))
+testAutocorrelQ1 = np.correlate(b1Q1norm, b1Q1norm, mode="full")
+testAutocorrelQ1 = testAutocorrelQ1[len(testAutocorrelQ1)//2:]
 
-PSDnorm = PSDlist/PSDmax
-    #if i == 0: PSDfreq.append(0)
-    #else: PSDfreq.append((1/i*tau)*60)
+testAutocorrelQ2 = np.correlate(b1Q2norm, b1Q2norm, mode="full")
+testAutocorrelQ2 = testAutocorrelQ2[len(testAutocorrelQ2)//2:]
 
-testAutocorrel = np.correlate(b1norm,b1norm,mode="full")
-testAutocorrel = testAutocorrel[len(testAutocorrel)//2:]
-averageautocorr = sum(testAutocorrel)/len(testAutocorrel)
+#soustraction de la moyenne des valeurs à toutes les valeurs :
+averageautocorr = sum(testAutocorrelQ1)/len(testAutocorrelQ1)
+testAutocorrelQ1 = testAutocorrelQ1 - averageautocorr
 
-testAutocorrel = testAutocorrel - averageautocorr
+averageautocorr = sum(testAutocorrelQ2)/len(testAutocorrelQ2)
+testAutocorrelQ2 = testAutocorrelQ2 - averageautocorr
 
-plot_acf(btot, lags=np.arange(len(btot)))
+
+#Autre test de plot direct d'une autocorrélation normalisée
+#plot_acf(btotQ1, lags=np.arange(len(btot)))
 
 
 #print(len(PSDlist))
@@ -163,23 +194,36 @@ plot_acf(btot, lags=np.arange(len(btot)))
 bar.finish()
 plt.show()
 
-plt.plot(b1norm)
+plt.plot(b1Q1norm)
+plt.plot(b1Q2norm)
+plt.title("Plot de b pour une bactérie")
 plt.show()
 
-plt.plot(testAutocorrel)
+plt.plot(testAutocorrelQ1)
+plt.plot(testAutocorrelQ2)
+plt.title("Autocorrelation")
 plt.show()
 
 #print(len(tot_freq))
 
-plt.plot(PSDnorm)
-plt.show()
+#plt.plot(PSDnorm)
+#plt.show()
+#calcul de la transformée de fourier sur l'autocorrelation
+fourierQ1=np.fft.fft(testAutocorrelQ1)
+fourierQ2=np.fft.fft(testAutocorrelQ2)
 
-fourier=np.fft.fft(testAutocorrel)
-freqfour=np.fft.fftfreq(len(testAutocorrel),d=tau)
-print(fourier)
-print(freqfour)
+
+freqfour=np.fft.fftfreq(len(testAutocorrelQ1),d=tau)
+#print(fourier)
+#print(freqfour)
+
+#Plot de la transfo de fourier
+plt.plot(abs(freqfour), abs(fourierQ1), label = "Q = 1")
+plt.plot(abs(freqfour), abs(fourierQ2), label = "Q = 0")
 plt.xlabel('Frequence')
 plt.ylabel('PSD')
 plt.title("Fourier Transform")
-plt.plot(abs(freqfour), abs(fourier))
+plt.legend()
+plt.xlim(0,0.2)
+
 plt.show()
