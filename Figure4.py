39e021c1cdd419cf702af3af2c1ac0101647e0dc
Wabognie <tiphaine.casy@hotmail.fr>
2020-12-05 12:03:55 +0100

    pull

--- Figure4.py
+++ Figure4.py
@@ -7,22 +7,24 @@ from progress.bar import Bar
 import statsmodels
 from statsmodels.graphics.tsaplots import plot_acf
 
-nCell = 100
+nCell = 10
 ##constantes
 n = 2
 alpha = 111
 kappa = 20
 k_s0 = 1
 k_s1 = 0.01
-t = 5000
-eta = 10
+t = 2000
+eta = 2
 
-D = 80
+
 
 beta = []
 for i in range(nCell):
     beta.append(random.gauss(1,0.05))
-tau = 0.05 ##a voir
+
+tau = 0.35 ##a voir
+D = 0.6/tau
 
 '''
 Réalisation de la simulation pour Q = 1, les cellules sont en synchro parfaite.
@@ -42,34 +44,39 @@ C = np.zeros((nCell, t))
 S = np.zeros((nCell, t))
 Se = np.zeros((nCell, t))
 
-a[:,0] = 0
-bQ1[:,0] = 50
-c[:,0] = 0
+for cell in range(nCell):
+    bQ1[cell,0] = random.randint(50, 100)
+    a[cell,0] = random.randint(50, 100)
+    c[cell,0] = random.randint(50, 100)
 
-A[:,0] = 0
-B[:,0] = 0
-C[:,0] = 0
+    A[cell,0] = random.randint(50, 100)
+    B[cell,0] = random.randint(50, 100)
+    C[cell,0] = random.randint(50, 100)
 
-S[:,0] = 0
-Se[:,0] = 0
+    S[cell,0] = random.randint(50, 100)
+    Se[cell,0] = random.randint(50, 100)
 
 
 time = np.arange(0,t)
 
 for j in range(0, t-1):
     for i in range(0, nCell):
-        xi = random.gauss(0, D)
-        betabis = beta[i] + (xi*tau)
+        xiA = random.gauss(0, D)
+        betabisA = beta[i] + (xiA*tau)
 
-        Se[i, j] = Q * np.mean(S[:, j])
+        xiB = random.gauss(0, D)
+        betabisB = beta[i] + (xiB*tau)
+
+        xiC = random.gauss(0, D)
+        betabisC = beta[i] + (xiC*tau)
 
         abis = a[i, j] + tau / 2 * (-a[i, j] + (alpha / (1 + C[i, j] ** n)))
         bbis = bQ1[i, j] + tau / 2 * (-bQ1[i, j] + (alpha / (1 + A[i, j] ** n)))
         cbis = c[i, j] + tau / 2 * (-c[i, j] + (alpha / (1 + B[i, j] ** n)) + (kappa * S[i, j] / 1 + S[i, j]))
 
-        Abis = A[i, j] + tau / 2 * (betabis * (a[i, j] - A[i, j]))
-        Bbis = B[i, j] + tau / 2 * (betabis * (bQ1[i, j] - B[i, j]))
-        Cbis = C[i, j] + tau / 2 * (betabis * (c[i, j] - C[i, j]))
+        Abis = A[i, j] + tau / 2 * (betabisA * (a[i, j] - A[i, j]))
+        Bbis = B[i, j] + tau / 2 * (betabisB * (bQ1[i, j] - B[i, j]))
+        Cbis = C[i, j] + tau / 2 * (betabisC * (c[i, j] - C[i, j]))
 
         Sbis = S[i, j] + tau / 2 * ((-k_s0 * S[i, j]) + (k_s1 * A[i, j]) - (eta * (S[i, j] - Se[i, j])))
 
@@ -77,18 +84,22 @@ for j in range(0, t-1):
         bQ1[i, j + 1] = bQ1[i, j] + tau * (-bbis + (alpha / (1 + Abis ** n)))
         c[i, j + 1] = c[i, j] + tau * (-cbis + (alpha / (1 + Bbis ** n)) + (kappa * Sbis / 1 + Sbis))
 
-        A[i, j + 1] = A[i, j] + tau * (betabis * (abis - Abis))
-        B[i, j + 1] = B[i, j] + tau * (betabis * (bbis - Bbis))
-        C[i, j + 1] = C[i, j] + tau * (betabis * (cbis - Cbis))
+        A[i, j + 1] = A[i, j] + tau * (betabisA * (abis - Abis))
+        B[i, j + 1] = B[i, j] + tau * (betabisB * (bbis - Bbis))
+        C[i, j + 1] = C[i, j] + tau * (betabisC * (cbis - Cbis))
 
         S[i, j + 1] = S[i, j] + tau * ((-k_s0 * Sbis) + (k_s1 * Abis) - (eta * (Sbis - Se[i, j])))
 
+        Se[i, j+1] = Q * np.mean(S[:, j])
     bar.next()
 
 '''
 Réalisation de la simulation pour Q = 0, les cellules sont totalement en désynchro.
 '''
-Q = 0
+Q = 0.2
+beta = []
+for i in range(nCell):
+    beta.append(random.gauss(1,0.05))
 
 bar = Bar("Progress", max=t-1)
 ##initialisation des matrices
@@ -103,34 +114,40 @@ C = np.zeros((nCell, t))
 S = np.zeros((nCell, t))
 Se = np.zeros((nCell, t))
 
-a[:,0] = 0
-bQ2[:,0] = 50
-c[:,0] = 0
 
-A[:,0] = 0
-B[:,0] = 0
-C[:,0] = 0
+for cell in range(nCell):
+    bQ2[cell,0] = random.randint(50, 100)
+    a[cell,0] = random.randint(50, 100)
+    c[cell,0] = random.randint(50, 100)
 
-S[:,0] = 0
-Se[:,0] = 0
+    A[cell,0] = random.randint(50, 100)
+    B[cell,0] = random.randint(50, 100)
+    C[cell,0] = random.randint(50, 100)
+
+    S[cell,0] = random.randint(50, 100)
+    Se[cell,0] = random.randint(50, 100)
 
 
 time = np.arange(0,t)
 
 for j in range(0, t-1):
     for i in range(0, nCell):
-        xi = random.gauss(0, D)
-        betabis = beta[i] + (xi*tau)
+        xiA = random.gauss(0, D)
+        betabisA = beta[i] + (xiA*tau)
 
-        Se[i, j] = Q * np.mean(S[:, j])
+        xiB = random.gauss(0, D)
+        betabisB = beta[i] + (xiB*tau)
+
+        xiC = random.gauss(0, D)
+        betabisC = beta[i] + (xiC*tau)
 
         abis = a[i, j] + tau / 2 * (-a[i, j] + (alpha / (1 + C[i, j] ** n)))
         bbis = bQ2[i, j] + tau / 2 * (-bQ2[i, j] + (alpha / (1 + A[i, j] ** n)))
         cbis = c[i, j] + tau / 2 * (-c[i, j] + (alpha / (1 + B[i, j] ** n)) + (kappa * S[i, j] / 1 + S[i, j]))
 
-        Abis = A[i, j] + tau / 2 * (betabis * (a[i, j] - A[i, j]))
-        Bbis = B[i, j] + tau / 2 * (betabis * (bQ2[i, j] - B[i, j]))
-        Cbis = C[i, j] + tau / 2 * (betabis * (c[i, j] - C[i, j]))
+        Abis = A[i, j] + tau / 2 * (betabisA * (a[i, j] - A[i, j]))
+        Bbis = B[i, j] + tau / 2 * (betabisB * (bQ2[i, j] - B[i, j]))
+        Cbis = C[i, j] + tau / 2 * (betabisC * (c[i, j] - C[i, j]))
 
         Sbis = S[i, j] + tau / 2 * ((-k_s0 * S[i, j]) + (k_s1 * A[i, j]) - (eta * (S[i, j] - Se[i, j])))
 
@@ -138,57 +155,41 @@ for j in range(0, t-1):
         bQ2[i, j + 1] = bQ2[i, j] + tau * (-bbis + (alpha / (1 + Abis ** n)))
         c[i, j + 1] = c[i, j] + tau * (-cbis + (alpha / (1 + Bbis ** n)) + (kappa * Sbis / 1 + Sbis))
 
-        A[i, j + 1] = A[i, j] + tau * (betabis * (abis - Abis))
-        B[i, j + 1] = B[i, j] + tau * (betabis * (bbis - Bbis))
-        C[i, j + 1] = C[i, j] + tau * (betabis * (cbis - Cbis))
+        A[i, j + 1] = A[i, j] + tau * (betabisA * (abis - Abis))
+        B[i, j + 1] = B[i, j] + tau * (betabisB * (bbis - Bbis))
+        C[i, j + 1] = C[i, j] + tau * (betabisC * (cbis - Cbis))
 
         S[i, j + 1] = S[i, j] + tau * ((-k_s0 * Sbis) + (k_s1 * Abis) - (eta * (Sbis - Se[i, j])))
 
+        Se[i, j+1] = Q * np.mean(S[:, j])
     bar.next()
 
 
 fourierQ1list = []
 fourierQ2list = []
-
+autoQ1 = []
+autoQ2 = []
 for cell in range(nCell):
     #Centralisation et normalisation de b
-    b1Q1norm = (bQ1[cell, :] - (max(bQ1[cell, :])/2))/(max(bQ1[cell, :])/2)
+    b1Q1norm = (bQ1[cell, 0:2000] - (np.mean(bQ1[cell, 0:2000])))/(np.var(bQ1[cell, 0:2000]))
 
-    b1Q2norm = (bQ2[cell, :] - (max(bQ2[cell, :])/2))/(max(bQ2[cell, :])/2)
+    b1Q2norm = (bQ2[cell, 0:2000] - (np.mean(bQ2[cell, 0:2000])))/(np.var(bQ2[cell, 0:2000]))
 
-    #Autocorrelation des valeurs : calcul la somme des multiplications des valeurs aux temps avec un décallage (b(t) * b(t+i))
-    autocorrelQ1 = np.correlate(b1Q1norm, b1Q1norm, mode="full")
-    autocorrelQ1 = autocorrelQ1[len(autocorrelQ1)//2:]
+    autoCorrelQ1 = np.correlate(b1Q1norm, b1Q1norm, mode="full")
+    autoCorrelQ1 = autoCorrelQ1[autoCorrelQ1.size//2:]
 
-    autocorrelQ2 = np.correlate(b1Q2norm, b1Q2norm, mode="full")
-    autocorrelQ2 = autocorrelQ2[len(autocorrelQ2)//2:]
+    autoCorrelQ2 = np.correlate(b1Q2norm, b1Q2norm, mode="full")
+    autoCorrelQ2 = autoCorrelQ2[autoCorrelQ2.size//2:]
 
-    #soustraction de la moyenne des valeurs à toutes les valeurs :
-    averageautocorr = sum(autocorrelQ1)/len(autocorrelQ1)
-    autocorrelQ1 = autocorrelQ1 - averageautocorr
-
-    averageautocorr = sum(autocorrelQ2)/len(autocorrelQ2)
-    autocorrelQ2 = autocorrelQ2 - averageautocorr
-
+    autoQ1.append(autoCorrelQ1)
+    autoQ2.append(autoCorrelQ2)
     #Réalisation des transformées de fourier et récupération dans une liste
-    fourierQ1list.append(np.fft.fft(autocorrelQ1))
-    fourierQ2list.append(np.fft.fft(autocorrelQ2))
+    fourierQ1list.append(np.fft.fft(autoCorrelQ1))
+    fourierQ2list.append(np.fft.fft(autoCorrelQ2))
 
 #calcul des fréquences fft
-freqfour=np.fft.fftfreq(len(autocorrelQ1),d=tau)
-
-bar.finish()
-plt.show()
-
-plt.plot(b1Q1norm)
-plt.plot(b1Q2norm)
-plt.title("Plot de b pour une bactérie")
-plt.show()
-
-plt.plot(autocorrelQ1)
-plt.plot(autocorrelQ2)
-plt.title("Autocorrelation")
-plt.show()
+freqfour=np.fft.fftfreq(len(autoCorrelQ2))
+print(freqfour)
 
 
 #calcul de la moyenne des fft:
@@ -197,20 +198,20 @@ fourierQ2 = np.array([np.mean(k) for k in zip(*fourierQ2list)])
 
 
 #Plot de la transfo de fourier
-plt.plot(abs(freqfour), abs(fourierQ1), label = "Q = 1")
-plt.plot(abs(freqfour), abs(fourierQ2), label = "Q = 0")
+plt.plot(abs(freqfour), np.real(fourierQ1), label = "Q = 1")
+plt.plot(abs(freqfour), np.real(fourierQ2), label = "Q = 0")
 plt.xlabel('Frequence')
 plt.ylabel('PSD')
+plt.yscale('log')
 plt.title("Fourier Transform Mean")
 plt.legend()
-plt.xlim(0,0.2)
+plt.xlim(0,0.1)
+plt.ylim(0.1, 1000)
 plt.show()
 
-plt.plot(abs(freqfour), abs(fourierQ1list[1]), label = "Q = 1")
-plt.plot(abs(freqfour), abs(fourierQ2list[1]), label = "Q = 0")
-plt.xlabel('Frequence')
-plt.ylabel('PSD')
-plt.title("Fourier Transform for 1 cell")
-plt.legend()
-plt.xlim(0,0.2)
+
+plt.imshow(np.transpose(fourierQ1list), extent = [0,100,0,0.3], aspect= 'auto', vmin=0)
+plt.show()
+
+plt.imshow(np.transpose(fourierQ2list), extent = [0,100,0,0.3], vmin=0, aspect='auto')
 plt.show()
